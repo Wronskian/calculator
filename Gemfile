ff261385b842562c44fe9cf19015252e45bdc70a
Ryan Dalke <rdalke@gmail.com>
2011-03-06 15:25:24 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.4'
+gem 'devise'
+gem 'bcrypt-ruby'
+gem 'jquery-rails', :git => 'git://github.com/pgericson/jquery-rails.git'
+
+gem 'sqlite3-ruby', :group => :development
+
+group :test, :development do
+  gem 'rspec-rails'
+  gem 'webrat'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mathemagic::Application.config.secret_token = 'f8b720777673c6fad697925d652008b44276a67354a12c33d062fc7da8ce7a0e2454257a60e9ca4191b37ed2640f3301b61680f4a9c9e12653e61a87852a22df'
