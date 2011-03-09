5d1b4d38b51e2273769916e7113929746ca5b3c9
Ryan Dalke <rdalke@gmail.com>
2011-03-09 00:59:17 -0700

    Added settings, math and matrix panels

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,25 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function(){
+  $(".settings_toggle").click(function(){
+    $(this).toggleClass("selected");
+    $(".settings_panel").slideToggle("slow");
+  });
+  
+  $(".mode_option").click(function(){
+    $(".mode_option").removeClass("selected");
+    $(".mode_panel").hide();
+  });
+  
+  $(".math_option").click(function(){
+    $(this).addClass("selected");
+    $(".math_panel").show();
+  });
+  
+  $(".matrix_option").click(function(){
+    $(this).addClass("selected");
+    $(".matrix_panel").show();
+  });
+  
+});
